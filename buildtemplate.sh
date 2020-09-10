3aea2e2bbc48b02136d86499f58fc0525cc631af
flofriday <flohacksfriday@gmail.com>
2020-09-10 10:59:01 +0200

    Add pdf-metadata to math-de template

---
+++ buildtemplate.sh
@@ -0,0 +1,9 @@
+# A simple script to build the template
+mkhomework \
+-subject Math \
+-title "Homework #1" \
+-author flofriday \
+-duedate 10.09.2020 \
+-tasks "Problem A, Problem B, Problem C, Additional Task" \
+-output examples/math-de.tex \
+math-de.tmp
