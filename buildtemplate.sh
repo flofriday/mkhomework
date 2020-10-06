1408fe6460356a40722ff9421b9d14c83ff781e7
flofriday <flohacksfriday@gmail.com>
2020-10-06 16:37:19 +0200

    Add statistics homework template

--- buildtemplate.sh
+++ buildtemplate.sh
@@ -7,3 +7,13 @@ mkhomework \
 -tasks "Problem A, Problem B, Problem C, Additional Task" \
 -output examples/math-de.tex \
 math-de.tmp
+
+# A simple script to build the template
+mkhomework \
+-subject "Statistics and Probability" \
+-title "HW 1 - Task 1" \
+-author flofriday \
+-duedate 06.10.2020 \
+-tasks "not used" \
+-output examples/ws.tex \
+ws.tmp
